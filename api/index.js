7bcafed64ba04552ca36681c96507506eddd0602
NAGARAJS2002 <nr2277644@gmail.com>
2024-10-25 19:53:53 +0530

    connected mongodb

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import env from "dotenv";
+env.config()
 const app = express();
 const PORT = 3000;
 
+mongoose.connect(process.env.MONGODB_URL).then(() =>{
+    console.log('mongodb is connected')
+}).catch((error) => {
+console.log(error);
 
+})
 
 app.listen(PORT,() =>{
     console.log(`server is running on port ${PORT}`);
