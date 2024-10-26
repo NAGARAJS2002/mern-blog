eaeda096284b2c7b212c5da56507f0b8b1fc8e8e
NAGARAJS2002 <nr2277644@gmail.com>
2024-10-26 15:11:32 +0530

    complete signin functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function SignIn() {
+const [formData,setformData] = useState({});
+const [loading,setLoading] = useState(false);
+const [error,setError] = useState(null);
+const navigate = useNavigate()
+function handleChange(e) {
+  setformData({
+    ...formData,
+    [e.target.id]:e.target.value
+  })
+}
+
+const  handleSubmit = async (e) => {
+  e.preventDefault();
+  try {
+    const res = await fetch('/api/auth/signin',{
+      method: 'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify(formData),
+    });
+    
+    const data = await res.json();
+    if (data.success === false) {
+      setLoading(false);
+      setError(data.message)
+      return;
+    }
+    setLoading(false);
+    setError(null);
+    navigate('/')
+  } catch (error) {
+    setError(error)
+  }
+}
   return (
     <div className='min-h-screen mt-20'>
      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -15,20 +50,24 @@ export default function SignIn() {
   
  <div className='flex-1 '>
 
-  <form className='flex flex-col gap-5  ' >
+  <form className='flex flex-col gap-5  ' onSubmit={handleSubmit} >
    
     <input type="email"
     id="email"
     placeholder='name@gmail.com'
        className=' border p-2 rounded-lg outline-none focus:border-cyan-500'
+       onChange={handleChange}
      />
 
     <input type="password"
     id="password"
     placeholder='*****'
        className=' border p-2 rounded-lg outline-none focus:border-cyan-500'
+       onChange={handleChange}
     />
-    <button className='border p-2 rounded-lg  bg-gradient-to-r from-purple-500 to-pink-500 text-white '>Sign In</button>
+    <button  disabled={loading}className='border p-2 rounded-lg  bg-gradient-to-r from-purple-500 to-pink-500 text-white disabled:opacity/85 '>
+    {loading ? 'Loading...': "  Sign In"}
+      </button>
   </form>
   <div className='flex gap-2 text-sm mt-5'>
             <span>Dont Have an account?</span>
@@ -37,7 +76,8 @@ export default function SignIn() {
             </Link>
           </div>
 
- </div>
+    </div>
+    {error&& <p>{error}</p>}
  
     </div>
     </div>
