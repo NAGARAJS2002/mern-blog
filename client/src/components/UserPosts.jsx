fbaf6aa909cdc53fc408d0c1d4a1ae032de11ea2
NAGARAJS2002 <nr2277644@gmail.com>
2024-11-06 14:43:57 +0530

    add showmore functionality userpost results

--- client/src/components/UserPosts.jsx
+++ client/src/components/UserPosts.jsx
@@ -18,7 +18,9 @@ export default function UserPosts() {
             const data = await res.json();
             if (res.ok) {
               setUserPosts(data);
-              
+              if (data.length < 9) {
+                setShowMore(false)
+              }
             }
           } catch (error) {
             console.log(error.message);
@@ -28,6 +30,27 @@ export default function UserPosts() {
           fetchPosts();
         }
       }, [currentUser._id]);
+
+
+   const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`)
+        const data = await res.json();
+        if (res.ok) {
+            setUserPosts((prev)=>[...prev,...data]);
+            if (data.length <9) {
+                setShowMore(false)
+            }
+        }
+    } catch (error) {
+        console.log(error.message);
+        
+    }
+   }
+
+
+
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
     {currentUser._id && userPosts.length > 0 ? (
@@ -90,14 +113,14 @@ export default function UserPosts() {
             </Table.Body>
           ))}
         </Table>
-        {/* {showMore && (
+        {showMore && (
           <button
             onClick={handleShowMore}
             className='w-full text-teal-500 self-center text-sm py-7'
           >
             Show more
           </button>
-        )} */}
+        )}
       </>
     ) : (
       <p>You have no posts yet!</p>
